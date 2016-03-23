bc0d983be2191dbfacbf496ee5f39ffb4c749b5d
roman <rgnevashev@gmail.com>
2016-03-23 16:55:56 +0300

    update

--- package.js
+++ package.js
@@ -2,14 +2,14 @@
 Package.describe({
   name: 'rgnevashev:autoform-select2',
   summary: 'Custom select2 input type for AutoForm',
-  version: '4.0.2',
+  version: '4.0.2_1',
   git: 'https://github.com/rgnevashev/meteor-autoform-select2.git'
 });
 
 Package.onUse(function(api) {
   api.use([
     'templating@1.1.5',
-    'aldeed:template-extension@4.0.0',
+    'aldeed:template-extension@3.0.0 || 4.0.0',
     'aldeed:autoform@4.0.0 || 5.0.0',
     'rgnevashev:select2@4.0.2'
   ]);
